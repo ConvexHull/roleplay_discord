beb41b32fca8e9573baee18fe8570f2773f7807b
ConvexHull <originalbubu@gmail.com>
2018-01-12 17:49:10 +0900

    Custom dice + side fix + English

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ throwing a custom die several times.
 
     python prob_dice.py <Sum to Hit> <Number of Trials> <Number of Sides>
 
+### Examples
+
+    python dice_prob.py 12 2
+
+    >> Roughly 3% chances of rolling a 12 with 2 dice of 6 faces each
+
+    python dice_prob.py 20 4 10
+
+    >> Roughly 6% chances of rolling a 20 with 4 dice of 10 faces each
+
+    python dice_prob.py 5 3 "[-1, 0, 1, 2]"
+
+    >> Roughly 5% chances of rolling a 5 with 3 dice of 4 faces each
--- dice_prob.py
+++ dice_prob.py
@@ -1,4 +1,5 @@
 from scipy.stats import multinomial
+import json
 import numpy as np
 import sys
 
@@ -6,12 +7,14 @@ np.random.seed(0)
 
 query = int(sys.argv[1]) if len(sys.argv) > 1 else 3
 trials = int(sys.argv[2]) if len(sys.argv) > 2 else 1
-sides = int(sys.argv[3]) if len(sys.argv) > 3 else 6
-die = np.array(range(1, sides+1))
+sides = json.loads(sys.argv[3]) if len(sys.argv) > 3 else 6
+die = sides if isinstance(sides, list) else range(1, sides+1)
 
-rv = multinomial(n=trials, p=[1./sides]*sides)
-vals = rv.rvs(sides * 1000) * np.matrix(die).T
+# TODO There is a closed form we can use in most cases
+# http://mathworld.wolfram.com/Dice.html
+rv = multinomial(n=trials, p=[1./len(die)]*len(die))
+vals = rv.rvs(max(len(die) * 1000, 10000)) * np.matrix(die).T
 prob = (vals == query).sum() / 1. / len(vals)
 
-print 'Environ %d%% de chances d\'obtenir %d en %d jet(s) avec %d faces' % \
-      (round(prob*100), query, trials, sides)
+print 'Roughly %d%% chances of rolling a %d with %d dice of %d faces each' % \
+      (round(prob*100), query, trials, len(die))
